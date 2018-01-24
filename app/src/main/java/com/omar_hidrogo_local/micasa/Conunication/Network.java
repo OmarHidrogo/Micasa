85d6769e41104f1a14b9b3d80eaa38510cbf0ff6
Omar Hidrogo <omar.083090@gmail.com>
2018-01-23 18:12:21 -0600

    Modificacion para interaccion de Bluetooth e internet, Blutooth en funcionamiento

--- app/src/main/java/com/omar_hidrogo_local/micasa/Conunication/Network.java
+++ app/src/main/java/com/omar_hidrogo_local/micasa/Conunication/Network.java
@@ -1,13 +1,20 @@
 package com.omar_hidrogo_local.micasa.Conunication;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.view.View;
 
+import com.omar_hidrogo_local.micasa.MainActivity;
+
+import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 /**
  * Created by tmhidrooma on 27/10/2017.
@@ -15,17 +22,33 @@ import java.io.IOException;
 
 public class Network {
 
+    SharedPreferences miprefInternet = MainActivity.getContext().getSharedPreferences("cInternet", Context.MODE_PRIVATE);
+    String cInternet = miprefInternet.getString("cInternet", "");
     //Prendemos LED
     public void sendOn(){
         HttpClient httpclient = new DefaultHttpClient();
-        HttpPost httppost = new HttpPost("http://192.168.0.110/?L=1");
+
+        HttpPost httppost = new HttpPost("http://"+cInternet+"/?L=1");
         //Esta es nuestra url donde mandaremos el parametro uno
         try {
             //List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
             //Codigo anterior donde tenia el error, pero esto funciona si queremos hacer post
             //nameValuePairs.add(new BasicNameValuePair("L", "1"));
             //httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
-            httpclient.execute(httppost); // mandamos el parametro al sercidor
+            HttpResponse response =  httpclient.execute(httppost); // mandamos el parametro al sercidor
+
+            //Luego procesamos el contenido de la respuesta en formato InputStream Buffer y la paso a formato String
+            BufferedReader in = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+            StringBuffer sb = new StringBuffer("");
+            String line = "";
+            String NL = System.getProperty("line.separator");
+            while ((line = in.readLine()) != null) {
+                sb.append(line + NL);
+            }
+            in.close();
+
+            //Respuesta en formato String
+            String respsuesta = sb.toString();
         } catch (ClientProtocolException e) {
             // TODO Auto-generated catch block
         } catch (IOException e) {
@@ -36,14 +59,27 @@ public class Network {
     //Apagamos LED
     public void sendOff(View v){
         HttpClient httpclient = new DefaultHttpClient();
-        HttpPost httppost = new HttpPost("http://192.168.0.110/?");
+        HttpPost httppost = new HttpPost("http://"+cInternet+"/?");
         //URL donde lo apagamos ya que no envia ninguna variable, nosotros podriamos ponerle un 0
         //pero en el codigo del arduino hay que cambiarlo
         try {
             //List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
             //nameValuePairs.add(new BasicNameValuePair("L", "1"));
             // httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
-            httpclient.execute(httppost);
+            HttpResponse response =  httpclient.execute(httppost); // mandamos el parametro al sercidor
+
+            //Luego procesamos el contenido de la respuesta en formato InputStream Buffer y la paso a formato String
+            BufferedReader in = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+            StringBuffer sb = new StringBuffer("");
+            String line = "";
+            String NL = System.getProperty("line.separator");
+            while ((line = in.readLine()) != null) {
+                sb.append(line + NL);
+            }
+            in.close();
+
+            //Respuesta en formato String
+            String respsuesta = sb.toString();
         } catch (ClientProtocolException e) {
             // TODO Auto-generated catch block
         } catch (IOException e) {
